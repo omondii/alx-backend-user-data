14e283db3e7f30b35130fcda0e6799d891eeb076
Brian Omondi <omondiibrian00@gmail.com>
2024-02-16 00:31:41 +0300

    Basic User-object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,7 +3,8 @@
 from api.v1.auth.auth import Auth
 import base64
 import binascii
-from typing import Tuple
+from typing import Tuple, TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -55,3 +56,19 @@ class BasicAuth(Auth):
         cred = (email, pwd)
         return cred
         """
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'): # type: ignore
+        """ Returns the User instance based on email & password """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        user = User.search({"email": user_email})
+        try:
+            if user:
+                if user[0].is_valid_password(user_pwd):
+                    return user[0]
+                return None
+        except KeyError:
+            return None
