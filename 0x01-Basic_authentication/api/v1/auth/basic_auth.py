5bbe2e8979dcf36a37369aadc96c31e8aef589c1
Brian Omondi <omondiibrian00@gmail.com>
2024-02-15 19:02:30 +0300

    decode base64 data

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """ Basic Authentication """
 from api.v1.auth.auth import Auth
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -18,3 +20,15 @@ class BasicAuth(Auth):
 
         authed = authorization_header[authorization_header.find('Basic ')+6:]
         return authed
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """ Returns the decoded value of Base64 string """
+        if base64_authorization_header is None or base64_authorization_header is not\
+                isinstance(base64_authorization_header, str):
+            return None
+        
+        try:
+            decoded = base64.b64decode(base64_authorization_header)
+            return decoded.decode('utf-8')
+        except binascii.Error:
+            return None
