7d7cb65d60045197d66ce62a10016a9c0ec3bb9e
Brian Omondi <omondiibrian00@gmail.com>
2024-02-16 23:54:49 +0300

    session/user Id validation

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python3
 """ sessionAuth inherits from Auth """
 from api.v1.auth.auth import Auth
+import uuid
+from models.user import User
 
 
 class SessionAuth(Auth):
     """ Session Auth class """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Creates a session ID for a user_id """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        else:
+            sessionId = str(uuid.uuid4())
+            self.user_id_by_session_id[sessionId] = user_id
+            return sessionId
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ Returns a user Id based on sessionId """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        else:
+            UserId = self.user_id_by_session_id.get(session_id)
+            return UserId
