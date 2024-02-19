13047f4678e819c375bc215ab4913d18310f0c78
Brian Omondi <omondiibrian00@gmail.com>
2024-02-19 21:13:14 +0300

    Checks if pwd is hashed

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,3 +10,10 @@ def hash_password(password: bytes) -> bytes:
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ checks if a password is hashed """
+    pwd = password.encode('utf-8')
+    if bcrypt.checkpw(pwd, hashed_password):
+        return True
