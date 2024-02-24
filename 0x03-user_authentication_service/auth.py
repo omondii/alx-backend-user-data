6c272b2c56e8c5acee203ebc99a20ff83e153ad1
Brian Omondi <omondiibrian00@gmail.com>
2024-02-24 15:54:55 +0300

    reset session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -85,3 +85,12 @@ class Auth:
                 return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: str) -> None:
+        """ Updates the corresponding session_id """
+        try:
+            user = self._db.find_user_by(id=user_id)
+            self._db.update_user(user.id, session_id=None)
+            return None
+        except NoResultFound:
+            return None
