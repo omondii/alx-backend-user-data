c545aad35be7cb5d808e090827b65734b15d8811
Brian Omondi <omondiibrian00@gmail.com>
2024-02-24 22:07:36 +0300

    get profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -55,6 +55,18 @@ def logout() -> str:
         abort(403)
 
 
+@app.route("/profile", methods=['GET'])
+def profile():
+    """ Uses session id to retrieve user profile """
+    session_id = request.cookies.get('session_id')
+    if session_id:
+        user = AUTH.get_user_from_session_id(session_id)
+        if user:
+            return jsonify({"email": user.email}), 200
+        abort(403)
+    abort(403)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000',
             debug=True)
